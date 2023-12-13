429f09235482a4103a775566c999bb201c2220c4
Hector <heblasco@microsoft.com>
2023-12-13 12:58:35 +0100

    final

--- Application/src/RazorPagesTestSample/Data/Message.cs
+++ Application/src/RazorPagesTestSample/Data/Message.cs
@@ -6,7 +6,7 @@ namespace RazorPagesTestSample.Data
     public class Message
     {
         public int Id { get; set; }
-//Comment1
+//Comment12
         [Required]
         [DataType(DataType.Text)]
         [StringLength(250, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
